9b16c9919fd6840ad6bc79901a7e5b9f861baf0f
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-08-01 11:26:20 -0300

    Modulo 06: Conclusão

--- modulo06/src/config/ReactotronConfig.js
+++ modulo06/src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reacotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reacotron.configure()
+  const tron = Reacotron.configure({ host: '192.168.0.49' })
     .useReactNative()
     .connect();
 
--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
+export default class User extends React.Component {
+  static propTypes = PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired;
+
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+    const { stars } = this.state;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
