edce4c5ca3b0dd8b99efd2f3ec36667f57faf33d
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-08-01 17:04:37 -0300

    Modulo 07: criação do Header

---
+++ modulo07/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import Routes from './routes';
+
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ modulo07/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="Rocketshoes logo" />
+    </Container>
+  );
+}
