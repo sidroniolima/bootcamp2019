bf5be5e15372017be289b83b44dedc65f82db09b
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-08-02 17:06:23 -0300

    Modulo 07: início com Redux

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import './config/reactotron.config';
+
+import GlobalStyles from './styles/global';
 
 import Header from './components/Header';
 import Routes from './routes';
 
-import GlobalStyles from './styles/global';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- modulo07/src/components/Header/index.js
+++ modulo07/src/components/Header/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
 import logo from '../../assets/images/logo.svg';
 
-import { Container } from './styles';
+import { Container, Cart } from './styles';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
-      <img src={logo} alt="Rocketshoes logo" />
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes logo" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={26} color="#FFF" />
+      </Cart>
     </Container>
   );
 }
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
