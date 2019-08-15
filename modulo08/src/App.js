f2b516851552b99591d084c7ed9ada76e224b423
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-08-14 21:33:52 -0300

    Hooks: início

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
 
-  const [ techs, setTechs ] = useState([
-    'ReactJS',
-    'React Native'
-  ])
+  const handleAddTech = useCallback(() => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
 
-  const [ newTech, setNewTech ] = useState('')
+  useEffect(() => {
+    const storagedTechs = localStorage.getItem('techs');
+
+    if (storagedTechs) {
+      setTechs(JSON.parse(storagedTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techsSize = useMemo(() => techs.length, [techs]);
 
   return (
     <>
       <ul>
-      {techs.map( tech => (
-        <li>{tech}</li>
-      ))}
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
       </ul>
-      <input type="button" value={newTech} onChange={(e) => setNewTech(e.target.value)} />
+      <strong>Você tem {techsSize} tecnologias</strong>
+      <br />
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAddTech}>
+        Adicionar
+      </button>
     </>
   );
 }
