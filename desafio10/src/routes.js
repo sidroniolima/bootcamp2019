ccdb1241bf85cdc2a0ea77950ceefd8688b2f155
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-10-10 18:03:31 -0300

    Desafio 10: início

---
+++ desafio10/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    SignUp,
+  }),
+);
